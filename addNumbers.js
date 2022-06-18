6a3556810f6854e026925f576e39ce9bec8e166f
al Mohamady <al.Mohamady47@gmail.com>
2022-06-18 15:47:37 +0200

    06 modules II

---
+++ addNumbers.js
@@ -0,0 +1,9 @@
+const num1 = 4;
+const num2 = 13;
+
+function addValues() {
+
+    console.log(`total is : ${ num1 + num2 }`);
+}
+
+addValues();
--- app.js
+++ app.js
@@ -19,8 +19,12 @@ const names = require('./module1');
 const funcs = require('./functions');
 const objs = require('./objects1');
 
-console.log(objs);
+require('./addNumbers');
+
+/*console.log(objs);
 
 funcs("Ahmed Mohamady");
 funcs(names.name1);
-funcs(names.name2);
+funcs(names.name2);*/
+
+addValues();
