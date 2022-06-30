6ca1751915667614a6e487254c08d5e26c9388dd
al Mohamady <al.Mohamady47@gmail.com>
2022-06-30 20:40:15 +0200

    18 Event loop Ex

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
-const _ = require('lodash');
+/*const {readFile} = require('fs');
+console.log('Start the first Task');
 
-const item = [ 1, [2 , [3, [4]] ]];
-const getItems = _.flattenDeep(item);
-console.log(getItems);
-console.log("hi there, 22");
+readFile('./content/almohamady.txt', 'utf-8',(err, result) => {
+
+    if (err) {
+        console.log(err);
+        return;
+    } 
+    console.log(result);
+    console.log('Complete first task');
+});
+
+console.log('End system set the new task');
+
+setInterval(() => {
+    console.log('hi there');
+}, 2000);
+
+console.log('secound task => :)');*/
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+
+    console.log('request event');
+    res.end('Hello World');
+});
+
+server.listen(5000, () => {
+    console.log('server listening on port : 5000');
+});
