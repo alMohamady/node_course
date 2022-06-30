9a8b5209fb8ffc020d384fc366a15d168ff2d918
al Mohamady <al.Mohamady47@gmail.com>
2022-06-30 16:38:24 +0200

    15 pakages and git ignore

--- app.js
+++ app.js
@@ -1,20 +1,5 @@
-const http = require('http');
+const _ = require('lodash');
 
-const server = http.createServer((req, res) => {
-
-    if (req.url === '/') {
-        res.end('Welcome in the home page');
-    }
-    else if (req.url === '/about') {
-        res.end('hi there it about us');
-    } else { 
-
-        res.end (`
-            <h1> no content here </h1>
-            <p> sorry there's no content here <p/>
-            <a href="/"> to home </a> 
-        `);
-    }
-});
-
-server.listen(5000);
+const item = [ 1, [2 , [3, [4]] ]];
+const getItems = _.flattenDeep(item);
+console.log(getItems);
