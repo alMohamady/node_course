b7e0dad8825802ce82327904320480e77c831a8c
al Mohamady <al.Mohamady47@gmail.com>
2022-07-01 15:35:23 +0200

    21 acync pattern III util

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
-const {readFile} = require('fs');
+const {readFile, writeFile} = require('fs').promises;
 
-const getText = (pass) => {
-    return new Promise((resolve, reject) => {
-        readFile(pass, 'utf-8', (err, data) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(data);
-            }
-        });
-    })
-}
+//const util = require('util');
+//const readFileUtile = util.promisify(readFile);
+//const writeFileUtile = util.promisify(writeFile);
 
-/*getText('./content/almohamadyXxcz.txt')
-     .then((result) => console.log(result))
-     .catch((err) => console.log(err));*/
 const start = async() => {
     try {
-        const file1 = await getText('./content/almohamady.txt');
-        const file2 = await getText('./content/myFolder/test.txt');
+        const file1 = await readFile('./content/almohamady.txt', 'utf-8');
+        const file2 = await readFile('./content/myFolder/test.txt', 'utf-8');
         console.log(file1, file2);
+        await writeFile('./content/result-util.text'
+                           , `file1 : ${file1}, file 2 : ${file2}  : direct promises`
+                           , {flag: 'a'});
     }
     catch(err) {
         console.log(err);
