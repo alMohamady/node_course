5971140c7d50b03af2a1ccabc49290d4de382837
al Mohamady <al.Mohamady47@gmail.com>
2022-07-01 15:07:25 +0200

    21 acync pattern II

--- app.js
+++ app.js
@@ -12,6 +12,18 @@ const getText = (pass) => {
     })
 }
 
-getText('./content/almohamadyXxcz.txt')
+/*getText('./content/almohamadyXxcz.txt')
      .then((result) => console.log(result))
-     .catch((err) => console.log(err));
+     .catch((err) => console.log(err));*/
+const start = async() => {
+    try {
+        const file1 = await getText('./content/almohamady.txt');
+        const file2 = await getText('./content/myFolder/test.txt');
+        console.log(file1, file2);
+    }
+    catch(err) {
+        console.log(err);
+    }
+}
+
+start();
