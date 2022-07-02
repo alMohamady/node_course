47cc10acbff1437ac5879dcbe16a02383fc97df5
al Mohamady <al.Mohamady47@gmail.com>
2022-07-02 03:39:32 +0200

    28 HTTP request object

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ const http = require('http');
 
 const server = http.createServer( (req, res) => {
 
-   res.writeHead(200, {'content-type' : 'text/plain'});
-   res.write('<h1> Welcome in the home page </h1>');
-   res.end();
+   //console.log(req.method, req.url);
+   const url = req.url;
+   if (url === "/") {
+    res.writeHead(200, {'content-type' : 'text/html'});
+    res.write('<h1> Welcome in the home page </h1>');
+    res.end();
+   } else if (url === "/about") {
+    res.writeHead(200, {'content-type' : 'text/html'});
+    res.write('<h1> it\'s about us </h1>');
+    res.end();
+   } else {
+    res.writeHead(404, {'content-type' : 'text/html'});
+    res.write('<h2> 404 Error </h2>  <h1> sorry no content </h1>');
+    res.end();
+   }
 });
 
 server.listen(5000);
