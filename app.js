23016f652da8844d6932035c8740f6a27e092314
al Mohamady <al.Mohamady47@gmail.com>
2022-07-02 04:25:57 +0200

    29 HTTP with HTML

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 const http = require('http');
+const {readFileSync} = require('fs');
+
+const hp = readFileSync('./headers-css-master/header-1.html');
+
+const hpStyleMain = readFileSync('./headers-css-master/styles/reset.min.css');
+const hpStyle = readFileSync('./headers-css-master/styles/style.css');
+const hpHederCss = readFileSync('./headers-css-master/styles/header-1.css');
+const hpJs = readFileSync('./headers-css-master/js/header-1.js');
 
 const server = http.createServer( (req, res) => {
 
    //console.log(req.method, req.url);
    const url = req.url;
+   console.log(url);
    if (url === "/") {
     res.writeHead(200, {'content-type' : 'text/html'});
-    res.write('<h1> Welcome in the home page </h1>');
+    res.write(hp);
+    res.end();
+   } else  if (url === "/styles/reset.min.css") {
+    res.writeHead(200, {'content-type' : 'text/css'});
+    res.write(hpStyleMain);
+    res.end();
+   }else  if (url === "/styles/style.css") {
+    res.writeHead(200, {'content-type' : 'text/css'});
+    res.write(hpStyle);
+    res.end();
+   }else  if (url === "/styles/header-1.css") {
+    res.writeHead(200, {'content-type' : 'text/css'});
+    res.write(hpHederCss);
+    res.end();
+   } else  if (url === "/js/header-1.js") {
+    res.writeHead(200, {'content-type' : 'text/javascript'});
+    res.write(hpJs);
     res.end();
    } else if (url === "/about") {
     res.writeHead(200, {'content-type' : 'text/html'});
---
+++ headers-css-master/readme.md
@@ -0,0 +1,31 @@
+# Introducing Headers.css
+
+I got the idea of designing and implementing a blueprint website header components. The reason is that I can pick one and use it for a new project pretty quickly. I made 17 headers so far and aiming for more in the coming weeks. You can [check them out](https://headers-css.vercel.app/) here.
+
+[Read about building headers with flexbox](https://ishadeed.com/article/website-headers-flexbox/) on my blog.
+
+![](header-css.jpg)
+
+While working on them, I tried to focus on the following:
+
+- Simplicity
+- Fully responsive design
+- Used Sass so they can be easily edited (Still need to do some refactoring here and there)
+- Accessibility (Please open an issue if you spot something incorrectly)
+
+## How To Use
+
+I didn't build this project with a "smart" way to get the files. For now, you can get the HTML, Sass, and Javascript related for the header you want to check. I will try to enhance this.
+
+## Todo
+
+- [ ] Think about a build process
+- [ ] RTL Styling
+
+## Contributions
+
+Oh, that would be great and very nice of you. Please submit an issue to discuss or simply add a PR.
+
+## Issues
+
+Spotted a bug or something weird? Please [open an issue](https://github.com/shadeed/headers-css/issues). Thank you!
