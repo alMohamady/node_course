959dfdb91f47532af3834af2d4036547e3ce06d2
al Mohamady <al.Mohamady47@gmail.com>
2022-07-23 22:08:29 +0200

    32 JSON Basic

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 
-app.use(express.static('./headers-css-master'));
 
-/*app.get('/', (req, res) => {
+app.get('/', (req, res) => {
+   res.json([{name: "ahmed"}, {name: "mohamed"} ]);
+})
 
-   res.sendFile(path.resolve(__dirname, './headers-css-master/header-1.html'));
-
-});*/
-
-app.all('*', (req, res) => {
-
-   res.status(404).send('<h1> No content </h1>');
-});
-
-app.listen(5000, () => {
-
-   console.log('server run in port 5000');
-});
+app.listen(5000, () =>{
+   console.log('server on 5000');
+})
---
+++ my-data.js
@@ -0,0 +1,43 @@
+const users = [{
+    userId: 1,
+    jobTitleName: "Developer",
+    firstName: "Romin",
+    lastName: "Irani",
+    preferredFullName: "Romin Irani",
+    employeeCode: "E1",
+    region: "CA",
+    phoneNumber: "408-1234567",
+    emailAddress: "romin.k@gmail.com"
+},
+{
+    userId: 2,
+    jobTitleName: "Developer",
+    firstName: "Neil",
+    lastName: "Irani",
+    preferredFullName: "Neil Irani",
+    employeeCode: "E2",
+    region: "CA",
+    phoneNumber: "408-1111111",
+    emailAddress: "neilrira@gmail.com"
+},
+{
+    userId: 3,
+    jobTitleName: "Program Directory",
+    firstName: "Tom",
+    lastName: "Hanks",
+    preferredFullName: "Tom Hanks",
+    employeeCode: "E3",
+    region: "CA",
+    phoneNumber: "408-2222222",
+    emailAddress: "tomanks@gmail.com"
+}
+];
+
+const languages = [
+    {id: 1, name: "JavaScript"}, 
+    {id: 2, name: "C#"},
+    {id: 3, name: "Python"}, 
+    {id: 4, name: "Go"}
+]
+
+module.exports = { users, languages };
