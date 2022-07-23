392131baec1218c88c68a6a490718a0c8e112151
al Mohamady <al.Mohamady47@gmail.com>
2022-07-23 21:14:01 +0200

    33 SSR Server Side Rendering

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static('./public'))
+app.use(express.static('./headers-css-master'));
 
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
 
    res.sendFile(path.resolve(__dirname, './headers-css-master/header-1.html'));
 
-});
+});*/
 
 app.all('*', (req, res) => {
 
