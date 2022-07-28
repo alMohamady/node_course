d298bc63d17cc4e766d49d6f7722392d02c3290f
al Mohamady <al.Mohamady47@gmail.com>
2022-07-28 18:30:58 +0200

    36 Query String

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
-
+const {users} = require('./my-data');
 
 app.get('/', (req, res) => {
-   res.json([{name: "ahmed"}, {name: "mohamed"} ]);
-})
+   res.send('<h1> My Home </h1> <a href="/api/users"> get Users </a>');
+});
+
+
+app.get('/api/users', (req, res) => {
+   const myUsers = users.map((user) => {
+      const  {userId, firstName, emailAddress} = user;
+      return {userId, firstName, emailAddress} ;
+   });
+
+   res.json(myUsers);
+});
 
 app.listen(5000, () =>{
    console.log('server on 5000');
-})
+});
