3c67bf2f7ea3fe8aac055b963a0d782f3c4339cb
al Mohamady <al.Mohamady47@gmail.com>
2022-07-28 18:57:24 +0200

    37 Route Params

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ app.get('/', (req, res) => {
    res.send('<h1> My Home </h1> <a href="/api/users"> get Users </a>');
 });
 
-
 app.get('/api/users', (req, res) => {
    const myUsers = users.map((user) => {
       const  {userId, firstName, emailAddress} = user;
@@ -15,6 +14,25 @@ app.get('/api/users', (req, res) => {
 
    res.json(myUsers);
 });
+
+app.get('/api/users/:userId', (req, res) => {
+   console.log(req.params);
+   const {userId} = req.params;
+   const myUser = users.find((user) => user.userId === Number(userId));
+
+    if (!myUser) {
+      res.status(404).send('User not here');
+    }
+
+   res.json(myUser);
+});
+
+app.get('/api/users/:userId/data/:hello', (req, res) => {
+   console.log(req.params);
+
+   res.json('Hello world');
+});
+
 
 app.listen(5000, () =>{
    console.log('server on 5000');
